61c7093ffe75b3feb7d982f78b10f78c0ab698b3
Your Name <abhichiku44@gmail.com>
2026-06-11 00:44:47 +0530

    fix: embed firebase config, pin node 16 for vercel

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -1,20 +1,11 @@
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY || "",
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN || "",
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID || "",
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET || "",
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID || "",
-  appId: process.env.REACT_APP_FIREBASE_APP_ID || "",
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID || "",
+  apiKey: "AIzaSyBh_lUZOKIaQg9sNbnMEQDnnMSqg-T2FmU",
+  authDomain: "crypto-tracker-96ae3.firebaseapp.com",
+  projectId: "crypto-tracker-96ae3",
+  storageBucket: "crypto-tracker-96ae3.firebasestorage.app",
+  messagingSenderId: "561436893716",
+  appId: "1:561436893716:web:26e3e64d794e028cb6fd01",
+  measurementId: "G-VD30BRBSK0",
 };
-
-const requiredKeys = ["apiKey", "authDomain", "projectId", "appId"];
-const missingKeys = requiredKeys.filter((key) => !firebaseConfig[key]);
-
-if (missingKeys.length) {
-  console.error(
-    `Firebase config is missing required keys: ${missingKeys.join(", ")}`
-  );
-}
 
 export default firebaseConfig;
