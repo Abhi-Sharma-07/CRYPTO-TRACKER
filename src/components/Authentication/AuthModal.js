ac7665834956082f75534be6bd1d1b00f8a73525
Your Name <abhichiku44@gmail.com>
2026-06-14 01:55:18 +0530

    fix: remove unused CryptoState/setAlert from AuthModal - fixes Vercel CI build

--- src/components/Authentication/AuthModal.js
+++ src/components/Authentication/AuthModal.js
@@ -7,7 +7,6 @@ import Signup from "./Signup";
 import Login from "./Login";
 import { useState } from "react";
 import { auth } from "../../firebase";
-import { CryptoState } from "../../CryptoContext";
 import GoogleButton from "react-google-button";
 import {
   GoogleAuthProvider,
@@ -51,7 +50,6 @@ const modalLightTheme = createTheme({
 export default function AuthModal() {
   const classes = useStyles();
   const [open, setOpen] = useState(false);
-  const { setAlert } = CryptoState();
 
   const handleOpen = () => {
     setOpen(true);
